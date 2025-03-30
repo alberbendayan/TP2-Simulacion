4fa3be371ebc9178c3b47035c84b665856ae0c8d
stonefeld <theo.stanfield@gmail.com>
2025-03-30 13:16:08 -0300

    Mejoras interactivas para los gráficos en python
    Co-authored-by: Albert <abendayan@itba.edu.ar>
    Co-authored-by: tobi0412 <tveslosada@itba.edu.ar>

---
+++ python/graphic_a.py
@@ -0,0 +1,73 @@
+import json
+import os
+import sys
+
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def main():
+    if len(sys.argv) < 3:
+        print("Uso: python graphic_a.py <results_folder> <probability> [frame_skip] [show_animation]")
+        sys.exit(1)
+
+    results_path = sys.argv[1]
+    p = float(sys.argv[2])
+    frame_skip = int(sys.argv[3]) if len(sys.argv) > 3 else 10
+    show_animation = bool(sys.argv[4] if len(sys.argv) > 4 else "False")
+
+    data_path = os.path.join(results_path, f"{p:.4f}")
+    output_folder = "../graphics_results"
+
+    config = os.path.join(results_path, "config.json")
+
+    if not os.path.exists(config):
+        print("No se encontró el archivo de configuración:", config)
+        sys.exit(1)
+
+    with open(config, "r") as config_file:
+        config = json.load(config_file)
+
+    grid_size = config.get("gridSize", None)
+    probabilities = config.get("probabilities", [])
+
+    if grid_size is None or p not in probabilities:
+        print("El tamaño de la cuadrícula o la probabilidad seleccionada no están definidos en el archivo de configuración.")
+        sys.exit(1)
+
+    files = sorted([f for f in os.listdir(data_path) if f.endswith(".txt")])
+    if not files:
+        print("No se encontraron archivos en", data_path)
+        sys.exit(1)
+
+    matrices = []
+    for i, file in enumerate(files):
+        if i % frame_skip == 0:
+            filepath = os.path.join(data_path, file)
+            matrix = np.loadtxt(filepath, dtype=int)
+            matrices.append(matrix)
+
+    total_iterations = len(matrices)
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(matrices[0], cmap="Blues", vmin=-1, vmax=1)
+
+    def update(frame):
+        im.set_data(matrices[frame])
+        ax.set_title(f"Iteración {(frame + 1) * frame_skip}")
+        return (im,)
+
+    ani = animation.FuncAnimation(fig, update, frames=total_iterations, interval=1, blit=False, repeat=False)
+
+    os.makedirs(output_folder, exist_ok=True)
+    file_path = os.path.join(output_folder, f"animacionA_{p}_N{grid_size}.mp4")
+
+    ani.save(file_path, writer="ffmpeg", fps=100)
+
+    if show_animation:
+        plt.show()
+
+
+if __name__ == "__main__":
+    main()
